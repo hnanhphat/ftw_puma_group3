5aa9360baab451380831681d466a9043b670fc47
Dinh Le <lqdquangdinh@gmail.com>
2021-04-08 22:52:58 +0700

    retrieved data from API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import axios from 'axios'
 // const API_KEY = process.env.REACT_APP_API_KEY
 
 function App() {
+  const [issues, setIssues] = useState(null)
+
+  const getIssues = async () => {
+    try {
+
+      const res = await axios.get('https://api.github.com/repos/facebook/react/issues?page=$1&per_page=20')
+      // https://api.github.com/repos/${owner}/${repo}/issues?page=${pageNum}&per_page=20
+      setIssues(res.data)
+      console.log(res.data)
+    }
+    catch (err) {
+      setIssues("ERROR!")
+      console.log(issues)
+    }
+
+  }
+
+  useEffect(() => {
+    getIssues()
+  }, [])
+
   return (
     <div className="container">
 
