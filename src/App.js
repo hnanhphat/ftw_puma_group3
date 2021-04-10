fb4bea08d7079b5f32ff5d06c1786b7f36260c1a
Dinh Le <lqdquangdinh@gmail.com>
2021-04-10 21:35:12 +0700

    added pagination into master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import FirstView from './components/FirstView';
 import ItemList from './components/ItemList';
 import Footer from './components/Footer';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
+
 
 function App() {
   const [headerStatus, setHeaderStatus] = useState('');
@@ -13,6 +15,8 @@ function App() {
   const [owner, setOwner] = useState("");
   const [repo, setRepo] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  let currentPage = 1
+  const [loading, setLoading] = useState("")
 
   const handleSearch = (e) => {
     setSearchInput(e.target.value);
@@ -34,10 +38,11 @@ function App() {
 
   const getIssues = async () => {
     try {
-      const url = `https://api.github.com/repos/${owner}/${repo}/issues?page=1&per_page=20`;
+      const url = `https://api.github.com/repos/${owner}/${repo}/issues?page=${(currentPage)}&per_page=5`;
       const res = await fetch(url);
       const data = await res.json();
       if (res.status === 200) {
+        console.log('current page:', (currentPage))
         setIssues(data);
         setErrorMessage("");
         return;
@@ -50,7 +55,7 @@ function App() {
 
   useEffect(() => {
     window.onscroll = () => {
-      if(window.scrollY > 100) {
+      if (window.scrollY > 100) {
         setHeaderStatus('active');
       } else {
         setHeaderStatus('');
@@ -62,12 +67,21 @@ function App() {
     }
   }, [owner, repo]);
 
+  const handlePageClick = async (selectedObject) => {
+    setLoading("loading")
+    currentPage = (selectedObject.selected + 1)
+    console.log('page clicked:', (selectedObject.selected + 1))
+    setIssues("")
+    await getIssues()
+    setLoading("done")
+  };
+
   return (
     <div id="home">
       <Header
         status={headerStatus}
-        // page="https://github.com/facebook/react/"
-        // readme="https://github.com/facebook/react/blob/master/README.md"
+      // page="https://github.com/facebook/react/"
+      // readme="https://github.com/facebook/react/blob/master/README.md"
       />
       <main>
         <FirstView
@@ -77,7 +91,38 @@ function App() {
           searchInput={searchInput}
           handleSearch={handleSearch}
         />
-        <ItemList itemList={issues} />
+        {/* <ItemList itemList={issues} /> */}
+        <div className="issues">
+          {/* exchange "loding..." with a spinner in the future */}
+          {loading === "loading" ? <h1>loading...</h1>
+            : ''}
+
+          {issues ? <ItemList itemList={issues} />
+            : ""}
+
+          <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={20}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={4}
+            onPageChange={handlePageClick}
+            breakClassName={'page-item'}
+            breakLinkClassName={'page-link'}
+            containerClassName={'pagination'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            previousClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            activeClassName={'active'}
+          />
+
+
+        </div>
       </main>
       <Footer logo={logo} />
     </div>
